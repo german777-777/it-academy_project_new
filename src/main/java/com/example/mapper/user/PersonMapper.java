0f3c946041c20b61bf93ef3a63679ecdd9fbc8c5
German Pisarev <pisarevgerman78@gmail.com>
2022-10-18 13:58:45 +0300

    Development: update mapper for people

--- src/main/java/com/example/mapper/user/PersonMapper.java
+++ src/main/java/com/example/mapper/user/PersonMapper.java
@@ -3,20 +3,61 @@ package com.example.mapper.user;
 import com.example.dto.user.person.PersonRequestUpdateDto;
 import com.example.dto.user.person.PersonResponseDto;
 import com.example.dto.user.system.PersonRequestCreateDto;
+import com.example.exceptions.CreateEntityException;
 import com.example.model.users.Person;
+import com.example.model.users.Student;
+import com.example.model.users.Teacher;
+import com.example.model.users.credentials.Credentials;
 import com.example.model.users.roles.Role;
+import com.example.service.users.PersonService;
+import org.mapstruct.Mapper;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface PersonMapper {
-    Person toEntity(PersonRequestCreateDto personRequestCreateDto);
+@Mapper(componentModel = "spring")
+public abstract class PersonMapper {
+    protected PersonService personService;
 
-    Person toEntity(PersonRequestUpdateDto personRequestUpdateDto);
+    public Person toEntity(PersonRequestCreateDto personRequestCreateDto) {
+        Person person = switch (personRequestCreateDto.role()) {
+            case "Student" -> new Student();
+            case "Teacher" -> new Teacher();
+            default -> throw new CreateEntityException(" cause not correct role");
+        };
 
-    PersonResponseDto toDto(Person person);
+        person.setFirstName(personRequestCreateDto.firstName());
+        person.setLastName(personRequestCreateDto.lastName());
+        person.setPatronymic(personRequestCreateDto.patronymic());
+        person.setBirthDate(personRequestCreateDto.birthDate());
+        person.setCredentials(new Credentials(personRequestCreateDto.login(), personRequestCreateDto.password()));
 
-    List<PersonResponseDto> toListDtos(List<? extends Person> people);
+        return person;
+    }
 
-    UserDetails toUserDetails(String login, String password, List<Role> roles);
+    public Person toEntity(PersonRequestUpdateDto personRequestUpdateDto) {
+        Person person = personService.findById(personRequestUpdateDto.id());
+
+        person.setFirstName(personRequestUpdateDto.firstName());
+        person.setLastName(personRequestUpdateDto.lastName());
+        person.setPatronymic(personRequestUpdateDto.patronymic());
+        person.setBirthDate(personRequestUpdateDto.birthDate());
+        person.setCredentials(new Credentials(personRequestUpdateDto.login(), personRequestUpdateDto.password()));
+
+        return person;
+    }
+
+
+    public abstract PersonResponseDto toDto(Person person);
+
+    public abstract List<PersonResponseDto> toListDtos(List<? extends Person> people);
+
+    public UserDetails toUserDetails(String login, String password, List<Role> roles) {
+        List<SimpleGrantedAuthority> allAuthorities = new ArrayList<>();
+        roles.forEach(role -> allAuthorities.addAll(role.getAuthorities()));
+        return new User(login, password, true, true, true, true, allAuthorities);
+    }
 }
